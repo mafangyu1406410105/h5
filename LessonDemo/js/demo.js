fd25cf5ae4f49a2c54ff652e6ca06a37268e5a43
mafangyu1406410105 <1830750540@qq.com>
2017-11-18 18:50:08 +0800

    h5

---
+++ LessonDemo/js/demo.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="jquery-1.9.1.min.js" />
+$(function () {
+    var index;
+    $('.item').hover(function () {
+        index = $(this).index('.item');
+        $(this).addClass('active').siblings().removeClass('active');
+        var top = $(this).offset().top;
+        var left = $(this).offset().left;
+        var divOffest = { top: top, left: left };
+        divOffest.left += $(this).width() + 30;
+        $('.content').eq(index).css(divOffest).show();
+    }, function () {
+        setTimeout(function () {
+            $('.content').eq(index).hover(function () {
+                $(this).show();
+                $('.item').eq(index).addClass('active').siblings().removeClass('active');
+            }, function () {
+                $(this).hide();
+                $('.item').removeClass('active');
+            });
+        }, 50);
+        $('.item').removeClass('active');
+        $('.content').eq(index).hide();
+    });
+
+})
---
+++ Project0517/Project0517/js/demo.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="jquery-1.10.2.min.js" />
+$(function () {
+    $('.pic ul li').mouseover(function () {
+        $(this).stop(true).animate({ width: '789px' }, 1000).siblings().stop(true)
+        .animate({ width: '100px' }, 1000);
+    });
+})
